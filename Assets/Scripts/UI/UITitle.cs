a5ff3cccdc308018180a1f908985156aab800523
bobbyliu <bobbyliu7303@gmail.com>
2020-11-07 23:53:44 -0800

    Further cleanup
    Cleanup STicky.cs and Title.cs.

--- Assets/Scripts/UI/UITitle.cs
+++ Assets/Scripts/UI/UITitle.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UITitle : MonoBehaviour
 {
@@ -14,5 +16,18 @@ public class UITitle : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void LoadMainScene()
+    {
+        StartCoroutine(LoadMainSceneCoroutine());
+        this.GetComponent<Button>().enabled = false;
+    }
+    private IEnumerator LoadMainSceneCoroutine()
+    {
+        float fadeTime = this.GetComponent<Fading>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene("LevelSelection");
+        yield break;
     }
 }
